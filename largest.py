11f1956ec9f3ac7d866ad45ab7d0e47ce6979833
arju-vk <arjunvkumar888@gmail.com>
2025-11-27 23:36:12 +0530

    python

---
+++ largest.py
@@ -0,0 +1,27 @@
+num1=110
+num2=45
+num3=189
+
+if (num1>num2) or (num1>num3):
+    print("num1")
+    
+elif (num2>num3):
+    
+    print("num2")
+    
+else:
+    print("num3")
+
+
+'''
+num1=100
+num2=200
+num3=400
+
+if (num1>num2) or (num1>num3):
+    print("num1 is greatest")
+elif (num2>num3):
+    print("num2 is greatest")
+else:
+    print("num3 is greatest")
+    '''
