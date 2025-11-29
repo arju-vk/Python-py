a5fe52c70cda932e4b22321775d203a17235e02f
arju-vk <arjunvkumar888@gmail.com>
2025-11-29 17:57:35 +0530

    py

--- break.py
+++ break.py
@@ -11,8 +11,18 @@
 #     if i == 3:
 #         break
 
-for i in range(1,6):
-    for j in range(1,6):
-        print(i*j)
-    if i == 3:
-        break
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(i*j)
+#     if i == 3:
+#         break
+
+n = int(input("Number : "))
+lst = []
+_sum = 0
+for _ in range(n):
+    x = int(input("NUM : "))
+    lst.append(x)
+    _sum += x
+print(lst)
+print(_sum)
