cee464c730ebb9a5308ef260efeef78d71fb26d7
arju-vk <arjunvkumar888@gmail.com>
2025-11-29 18:09:56 +0530

    py

--- break.py
+++ break.py
@@ -17,12 +17,36 @@
 #     if i == 3:
 #         break
 
+# n = int(input("Number : "))
+# lst = []
+# _sum = 0
+# for _ in range(n):
+#     x = int(input("NUM : "))
+#     lst.append(x)
+#     _sum += x
+# print(lst)
+# print(_sum)
+
+
+
+
+
 n = int(input("Number : "))
 lst = []
-_sum = 0
-for _ in range(n):
-    x = int(input("NUM : "))
-    lst.append(x)
-    _sum += x
-print(lst)
-print(_sum)
+odd = 0
+even = 0
+
+# for _ in range(n):
+#     x = int(input("NUM : "))
+#     lst.append(x)
+#     
+#     if x%2==0:
+#         even+= x
+#     else:
+#         odd+=x
+# 
+# print(lst)
+# print("Sum of the even numbers:", even)
+# print("sum of the odd numbers :",odd )
+
+
