18a8da4cdbf54adeedb2e285b01c93a9241f32a1
arju-vk <arjunvkumar888@gmail.com>
2025-12-02 17:05:53 +0530

    py

--- dict.py
+++ dict.py
@@ -5,12 +5,12 @@ for key in d:
     print(key)
 
 # # Iterate over values
-# for value in d.values():
-#     print(value)
-# 
-# # Iterate over key-value pairs
-# for key, value in d.items():  
-#     print(f"{key}: {value}")
+for value in d.values():
+    print(value)
+
+# Iterate over key-value pairs
+for key, value in d.items():  
+    print(f"{key}: {value}")
 
 
 
@@ -23,31 +23,31 @@ for key in d:
 
 
 
-# d = {1: 'Rahul', 2: 'Ramu', 3: 'Raj', 'age':22}
-# 
-# # Using del to remove an item
-# del d["age"]
-# print(d)
-# 
-# # Using pop() to remove an item and return the value
-# val = d.pop(1)
-# print(val)
-# 
-# # Using popitem to removes and returns
-# # the last key-value pair.
-# key, val = d.popitem()
-# print(f"Key: {key}, Value: {val}")
-# 
-# # Clear all items from the dictionary
-# d.clear()
-# print(d)
+d = {1: 'Rahul', 2: 'Ramu', 3: 'Raj', 'age':22}
+
+# Using del to remove an item
+del d["age"]
+print(d)
+
+# Using pop() to remove an item and return the value
+val = d.pop(1)
+print(val)
+
+# Using popitem to removes and returns
+# the last key-value pair.
+key, val = d.popitem()
+print(f"Key: {key}, Value: {val}")
+
+# Clear all items from the dictionary
+d.clear()
+print(d)
 
 
 
-# d = { "name": "Prakash", 1: "Python", (1, 2): [1,2,4] }
-# 
-# # Access using key
-# print(d["name"])
-# 
-# # Access using get()
-# print(d.get(1))
+d = { "name": "Prakash", 1: "Python", (1, 2): [1,2,4] }
+
+# Access using key
+print(d["name"])
+
+# Access using get()
+print(d.get(1))
