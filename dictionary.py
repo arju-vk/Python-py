dd8cc0da67bf0b989dc1d3dd3d9da647e7a619c4
arju-vk <arjunvkumar888@gmail.com>
2025-12-02 18:51:13 +0530

    py

--- dictionary.py
+++ dictionary.py
@@ -3,3 +3,12 @@ num = int(input("Enter the number of elements: "))
 for i in range(num):
     value = input("Enter Value: ")
     di[i] = value
+print((di))
+for j in di:
+    if j==1:
+        print(di.keys())
+        break
+
+
+
+    
