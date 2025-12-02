7da76c844de40bc8ccb7d00e599e5d806e312c20
arju-vk <arjunvkumar888@gmail.com>
2025-12-02 18:23:25 +0530

    py

---
+++ di.py
@@ -0,0 +1,15 @@
+di ={'a':1,'b':2,'c':3,'d':4,'e':5}
+print(di)
+di.update({'f':6})
+print(di)
+di.pop('c')
+print(di)
+di['b']=20
+print(di)
+di[z]=26
+print(di)
+di.popitem()
+print(di)
+di.clear()
+print(di)
+
